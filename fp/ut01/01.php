1a0db0ed5ffc0f88e8dbe1ce32d140c590dd2fbb
Hamza Mohammad <hamzzza56@gmail.com>
2020-10-30 10:19:52 +0100

    Comienza repositorio y termina ut01/01.php

---
+++ fp/ut01/01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP.
+-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo 'Hamza Mohammad';
+    ?>
+</body>
+</html>
