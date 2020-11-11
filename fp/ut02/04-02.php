2aa6d4c545b1cc1125d391f017b6d480ed287158
Hamza Mohammad <hamzzza56@gmail.com>
2020-11-11 11:29:37 +0100

    Termina ut02-04

--- fp/ut02/04-02.php
+++ fp/ut02/04-02.php
@@ -1,2 +1,17 @@
 <!-- Escribe un programa que sume, reste, multiplique y divida 
     dos números introducidos por teclado. -->
+<?php
+
+    $numero1 = (int)$_POST['numero1'];
+    $numero2 = (int)$_POST['numero2'];
+
+    $suma = $numero1 + $numero2;
+    $resta = $numero1 - $numero2;
+    $multi = $numero1 * $numero2;
+    $div = $numero1 / $numero2;
+
+    echo 'Suma = ' . $suma . '<br>';
+    echo 'Resta = ' . $resta . '<br>';
+    echo 'Multliplicación = ' . $multi . '<br>';
+    echo 'División = ' . $div . '<br>';
+?>
